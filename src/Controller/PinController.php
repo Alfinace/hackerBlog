cda58e588388f7309c256d0cc5cf7cadd4c575c0
=Alfinace <nantenainaalfinace@gmail.com>
2020-08-06 17:42:33 +0300

    Add config, list index pin by createdAt

--- src/Controller/PinController.php
+++ src/Controller/PinController.php
@@ -2,7 +2,7 @@
 /*
  * @Author: your name
  * @Date: 2020-08-04 23:30:31
- * @LastEditTime: 2020-08-05 00:44:32
+ * @LastEditTime: 2020-08-06 17:37:40
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /cours-symfony-container/src/Controller/PinController.php
@@ -10,19 +10,36 @@
 
 namespace App\Controller;
 
+use App\Entity\Pin;
 use App\Repository\PinRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PinController extends AbstractController
 {
     /**
-     * @Route("/pin", name="pin")
+     * @Route("/", name="app_index_pin")
+     * 
+     * @param PinRepository $pinRepository
+     * @return Response
      */
-    public function index(PinRepository $pinRepository)
+    public function index(PinRepository $pinRepository):Response
     {
         return $this->render('pin/index.html.twig', [
-            'pins' => $pinRepository->findAll()
+            'pins' => $pinRepository->findBy([],['createdAt'=>'DESC'])
         ]);
+    }
+    
+    /**
+     * @Route("/show/{id<[0-9]+>}", name="app_show_pin",methods={"GET"})
+     *
+     * @param Pin $pin
+     * @return Response
+     */
+    public function show(Pin $pin):Response
+    {
+        
+        return $this->render('pin/show.html.twig',compact("pin"));
     }
 }
--- src/Entity/Pin.php
+++ src/Entity/Pin.php
@@ -2,7 +2,7 @@
 /*
  * @Author: your name
  * @Date: 2020-08-04 23:28:14
- * @LastEditTime: 2020-08-04 23:31:16
+ * @LastEditTime: 2020-08-06 17:12:10
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /cours-symfony-container/src/Entity/Pin.php
@@ -15,6 +15,7 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ORM\Entity(repositoryClass="App\Repository\PinRepository")
  * @ORM\Table(name="pins")
+ * @ORM\HasLifecycleCallbacks
  */
 class Pin
 {
@@ -34,6 +35,16 @@ class Pin
      * @ORM\Column(type="text", length=255)
      */
     private $description;
+
+    /**
+     * @ORM\Column(type="datetime", options={"default":"CURRENT_TIMESTAMP"})
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime", options={"default":"CURRENT_TIMESTAMP"})
+     */
+    private $updatedAt;
 
     public function getId(): ?int
     {
@@ -62,5 +73,41 @@ class Pin
         $this->description = $description;
 
         return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * @ORM\PrePersist
+     * @ORM\PreUpdate
+     */
+    public function updateTimestamps()
+    {
+        if ($this->getCreatedAt()=== null) {
+            $this->setCreatedAt(new \DateTimeImmutable);
+        }
+        $this->setUpdatedAt(new \DateTimeImmutable);
     }
 }
